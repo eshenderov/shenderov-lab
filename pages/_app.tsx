6d779e47ffad3f5b923d441d055fbd93c31990b6
Alex Luo <alexbluo01@gmail.com>
2022-07-23 01:22:37 -0400

    clean up boilerplate and add linting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from "next/app";
 import "../index.css";
 
-function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
-}
+};
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import type { NextPage } from "next";
-import dynamic from "next/dynamic";
-
-const Test = dynamic(() => import("../components/test"), {ssr: false})
 
 const Home: NextPage = () => {
   return (
     <div className="h-screen w-screen">
-      <Test></Test>
+      wheee
     </div>
   );
 };
