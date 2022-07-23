b423bf5741944bd49316e838947654e026652d23
Alex Luo <alexbluo01@gmail.com>
2022-07-22 23:59:21 -0400

    add linting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import "./index.css";
+import "../index.css";
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from "next";
+import dynamic from "next/dynamic";
+
+const Test = dynamic(() => import("../components/test"), {ssr: false})
 
 const Home: NextPage = () => {
-  return <div className=""></div>;
+  return (
+    <div className="h-screen w-screen">
+      <Test></Test>
+    </div>
+  );
 };
 
 export default Home;
