b7cf55b3b14fdc587314051c2a6cac8f59a788ef
Alex Luo <alexbluo01@gmail.com>
2022-08-24 17:24:15 -0400

    center research text and add dummies, add gap between text and research images

--- components/ResearchSection.tsx
+++ components/ResearchSection.tsx
@@ -15,11 +15,26 @@ const ResearchSection = () => {
   return (
     <section id="research" className="relative">
       <h2>Research</h2>
-      <div ref={ref} className="grid grid-cols-2">
+      <div ref={ref} className="grid grid-cols-2 gap-16">
         <div className="">
-          <div className="h-screen"></div>
-          <div className="h-screen"></div>
-          <div className="h-screen"></div>
+          <div className="flex h-screen items-center">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
+            praesentium ad alias aliquid vero est laborum architecto nisi qui
+            harum modi nulla quod, amet provident? Asperiores doloribus
+            voluptates ab harum!
+          </div>
+          <div className="flex h-screen items-center">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam
+            ipsum ut fuga at soluta sapiente rem officia amet voluptatibus
+            accusamus, exercitationem omnis ullam assumenda numquam deleniti
+            iusto magni est officiis.
+          </div>
+          <div className="flex h-screen items-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            repudiandae excepturi dolor sequi, magnam quisquam quas tenetur
+            praesentium, aperiam rem laudantium eius vel impedit obcaecati
+            dignissimos enim magni debitis quos?
+          </div>
         </div>
         <div className="sticky top-0 h-screen">
           <div className="relative h-full">
