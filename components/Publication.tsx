6bc717047336e918ec76e43daf0572dd8cfc9fdd
Alex Luo <alexbluo01@gmail.com>
2022-09-07 09:00:19 -0400

    link publications on mobile

--- components/Publication.tsx
+++ components/Publication.tsx
@@ -23,28 +23,31 @@ const Publication = ({ title, uid, pubdate, journal, authors }: Props) => {
   };
 
   return (
-    // TODO: link on mobile
-    // TODO: set up domain
     <li className="flex gap-12 text-sm sm:text-base">
-      <div className="w-full">
-        <p
-          className="font-bold"
-          //  eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={createTitleMarkup()}
-        />
-        <p>{pubdate}</p>
-        <p>{journal}</p>
-        <p>
-          {authors.map(({ name }, i) => (
-            <span
-              className={name === "Shenderov E" ? "font-extrabold" : ""}
-              key={name}
-            >
-              {name + (i < authors.length - 1 ? ", " : "")}
-            </span>
-          ))}
-        </p>
-      </div>
+      <a
+        className="w-full sm:pointer-events-none"
+        href={`https://pubmed.ncbi.nlm.nih.gov/${uid}`}
+      >
+        <div className="w-full">
+          <p
+            className="font-bold"
+            //  eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={createTitleMarkup()}
+          />
+          <p>{pubdate}</p>
+          <p>{journal}</p>
+          <p>
+            {authors.map(({ name }, i) => (
+              <span
+                className={name === "Shenderov E" ? "font-extrabold" : ""}
+                key={name}
+              >
+                {name + (i < authors.length - 1 ? ", " : "")}
+              </span>
+            ))}
+          </p>
+        </div>
+      </a>
       <ButtonLink
         className="hidden xl:flex"
         href={`https://pubmed.ncbi.nlm.nih.gov/${uid}`}
