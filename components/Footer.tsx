a3907ca3ef60c8e5a262438902531126769313bb
Alex Luo <alexbluo01@gmail.com>
2022-09-20 19:17:41 -0400

    make footer links open in new tab

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,16 +9,32 @@ const Footer = () => {
   return (
     <footer className="flex w-full flex-col items-center justify-between gap-8 py-8 px-12 text-center text-white sm:py-16 sm:px-32 xl:px-48">
       <div className="flex items-center gap-8">
-        <a href="https://www.linkedin.com/in/eugene-shenderov-m-d-d-phil-facp-46384a7b/">
+        <a
+          href="https://www.linkedin.com/in/eugene-shenderov-m-d-d-phil-facp-46384a7b/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FontAwesomeIcon className="h-8 w-8" icon={faLinkedin} />
         </a>
-        <a href="https://twitter.com/doceshenderov">
+        <a
+          href="https://twitter.com/doceshenderov"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FontAwesomeIcon className="h-8 w-8" icon={faTwitterSquare} />
         </a>
-        <a href="https://www.hopkinsmedicine.org/profiles/details/eugene-shenderov">
+        <a
+          href="https://www.hopkinsmedicine.org/profiles/details/eugene-shenderov"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FontAwesomeIcon className="h-8 w-8" icon={faBuildingUser} />
         </a>
-        <a href="https://scholar.google.com/citations?user=YarQeqEAAAAJ&hl=en">
+        <a
+          href="https://scholar.google.com/citations?user=YarQeqEAAAAJ&hl=en"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FontAwesomeIcon className="h-8 w-8" icon={faBookOpen} />
         </a>
       </div>
--- components/FrontSection.tsx
+++ components/FrontSection.tsx
@@ -1,11 +1,8 @@
 const FrontSection = () => {
   return (
     <section id="front" className="h-screen">
-      {/* <div className="absolute bottom-0 left-0 -z-50 aspect-[900/600] w-full bg-[url('/layered-waves.svg')] bg-cover bg-center bg-no-repeat" /> */}
     </section>
   );
 };
-
-// TODO: logo in bg??? (preferably when almost done for rollback)
 
 export default FrontSection;
