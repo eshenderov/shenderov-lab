2498db031c49c901d5377f037fbe292fe70882c4
Alex Luo <alexbluo01@gmail.com>
2022-11-01 14:08:09 -0400

    add popup for team members

--- components/TeamMember.tsx
+++ components/TeamMember.tsx
@@ -1,22 +1,37 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 interface Props {
   name: string;
-  description: string;
   src: string;
+  children: string;
 }
 
-const TeamMember = ({ name, description, src }: Props) => {
+const TeamMember = ({ name, src, children }: Props) => {
+  const [visible, setVisible] = useState<boolean>(false);
+
   return (
-    <div className="flex-[0_0_50%] p-4 md:flex-[0_0_33%] lg:p-8 xl:flex-[0_0_25%] 2xl:flex-[0_0_20%]">
+    <button
+      className="relative flex-[0_0_50%] cursor-pointer items-center p-4 md:flex-[0_0_33%] lg:p-8 xl:flex-[0_0_25%] 2xl:flex-[0_0_20%]"
+      onClick={() => setVisible(true)}
+    >
       <div className="relative mb-4 aspect-square w-full overflow-hidden rounded-full">
-        <Image src={src} alt={`${name}, ${description}`} layout="fill" />
+        <Image src={src} alt={`${name}`} layout="fill" />
       </div>
-      <p className="text-center text-3xl font-medium text-peach">{name}</p>
-      <p className="text-center text-lg text-white text-opacity-90">
-        {description}
-      </p>
-    </div>
+      <p className="text-center text-2xl font-medium text-peach">{name}</p>
+      {/* TODO: change to modal */}
+      <motion.div
+        className="absolute top-1/2 left-1/2 p-8 w-full origin-bottom bg-peach"
+        animate={visible ? "visible" : "hidden"}
+        variants={{
+          visible: { opacity: 1, y: "-150%", x: "-50%" },
+          hidden: { opacity: 0, y:"-100%", x: "-50%" },
+        }}
+      >
+        {children}
+      </motion.div>
+    </button>
   );
 };
 
--- components/TeamSection.tsx
+++ components/TeamSection.tsx
@@ -6,8 +6,8 @@ import TeamMember from "./TeamMember";
 const TeamSection = () => {
   return (
     <section id="team">
-      <h2 className="pb-12 text-center font-outline text-5xl font-black text-green-apple sm:text-6xl lg:pb-24">
-        Meet The&nbsp;
+      <h2 className="pb-12 text-center font-outline text-5xl font-black text-green-apple sm:text-[4rem] lg:pb-24">
+        <span className="text-[4.25rem]">Meet The&nbsp;</span>
         <span className="bg-gradient-to-r from-dragonfruit to-peach bg-clip-text font-sans text-transparent">
           Team
         </span>
@@ -15,23 +15,31 @@ const TeamSection = () => {
       <div className="flex flex-wrap justify-center pb-12 lg:pb-24">
         {/* add team members inside here. top to bottom in code equates to left to right, top to bottom in the website */}
         {/* notice the / before the file name instead of the expected public/FILE_NAME */}
-        {/* Kelly: Kelly is a research technologist working closely with the GeoMx Digital Spatial Profiler for spatial transcriptomics and proteomics. She is currently pursuing a Master’s degree in Biotechnology with a concentration in Molecular Targets and Drug Discovery Technologies from Johns Hopkins University. In her spare time, she enjoys caring for her house plants. */}
-        {/* Adam is currently a third-year undergraduate at JHU majoring in
-        neuroscience. He is working on various projects ranging from the deep
-        learning-based diagnosis of leukemia to the spatial proteogenomics of
-        brain tumors. Fun fact: he has been in the lab for over four years
-        (since high school)! */}
-        {/* Justin is an undergraduate Hopkins student from San Diego majoring in
-        molecular and cellular biology. He hopes to gain research experience
-        while pursuing his interests in the medical field. When not in the lab,
-        Justin enjoys cooking and listening to music. */}
-        <TeamMember name="Alex" description="Alex" src="/alex.jpg" />
-        <TeamMember name="Alex" description="Alex" src="/alex.jpg" />
-        <TeamMember name="Alex" description="Alex" src="/alex.jpg" />
-        <TeamMember name="Alex" description="Alex" src="/alex.jpg" />
-        <TeamMember name="Alex" description="Alex" src="/alex.jpg" />
-        <TeamMember name="Alex" description="Alex" src="/alex.jpg" />
-        <TeamMember name="Alex" description="Alex" src="/alex.jpg" />
+        <TeamMember name="Adam Luo" src="/adam.jpg">
+          Adam is currently a third-year undergraduate at JHU majoring in
+          neuroscience. He is working on various projects ranging from the deep
+          learning-based diagnosis of leukemia to the spatial proteogenomics of
+          brain tumors. Fun fact: he has been in the lab for over four years
+          (since high school)!
+        </TeamMember>
+        <TeamMember name="Justin Hyun" src="/alex.jpg">
+          Justin is an undergraduate Hopkins student from San Diego majoring in
+          molecular and cellular biology. He hopes to gain research experience
+          while pursuing his interests in the medical field. When not in the
+          lab, Justin enjoys cooking and listening to music.
+        </TeamMember>
+        <TeamMember name="Kelly Casella" src="/alex.jpg">
+          Kelly is a research technologist working closely with the GeoMx
+          Digital Spatial Profiler for spatial transcriptomics and proteomics.
+          She is currently pursuing a Master’s degree in Biotechnology with a
+          concentration in Molecular Targets and Drug Discovery Technologies
+          from Johns Hopkins University. In her spare time, she enjoys caring
+          for her house plants.
+        </TeamMember>
+        <TeamMember name="Alex" src="/alex.jpg"></TeamMember>
+        <TeamMember name="Alex" src="/alex.jpg"></TeamMember>
+        <TeamMember name="Alex" src="/alex.jpg"></TeamMember>
+        <TeamMember name="Alex" src="/alex.jpg"></TeamMember>
       </div>
       <div className="flex flex-col gap-12 lg:flex-row lg:gap-16">
         <Gallery />
