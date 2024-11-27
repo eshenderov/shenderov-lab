aa88c08fa92c8ab182b1e2af083bc1c7460d0140
pangu99 <pgu7@wisc.edu>
2024-11-27 15:11:45 -0500

    Revert "add a scrollbar to the News section"
    This reverts commit b2d17ebd0482b112db5ddb3703e94d742bbbf6b5.
    back one step
     with '#' will be ignored, and an empty message aborts the commit.

--- components/NewsSection.tsx
+++ components/NewsSection.tsx
@@ -6,58 +6,35 @@ const NewsSection = () => {
       <h2 className="inline-block bg-gradient-to-r from-dragonfruit to-peach bg-clip-text pb-12 text-5xl font-black text-transparent sm:text-6xl">
         News
       </h2>
-      <style>
-        {`
-          /* Custom scrollbar for Webkit browsers (Chrome, Safari, Edge) */
-          .scrollable-container::-webkit-scrollbar {
-            height: 8px;
-          }
-
-          .scrollable-container::-webkit-scrollbar-track {
-            background: #f1f1f1;
-          }
-
-          .scrollable-container::-webkit-scrollbar-thumb {
-            background: #888;
-            border-radius: 8px;
-          }
-
-          .scrollable-container::-webkit-scrollbar-thumb:hover {
-            background: #555;
-          }
-        `}
-      </style>
-      <div className="scrollable-container overflow-x-auto max-w-full">
-        <div className="flex gap-12">
-          {/* add news blocks inside here. top to bottom in code equates to left to right, top to bottom in the website. */}
-          {/* <NewsBlock headline="This is a headline woohoo" date="August 18, 2022">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
-            oluptatum voluptatem aperiam? Quisquam, maiores animi alias aspernatur
-            voluptates, cumque rerum qui velit perspiciatis saepe possimus? Velit,
-            necessitatibus.
-          </NewsBlock>
-          <NewsBlock headline="This is a headline woohoo" date="August 18, 2022">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
-            oluptatum voluptatem aperiam? Quisquam, maiores animi alias aspernatur
-            voluptates, cumque rerum qui velit perspiciatis saepe possimus? Velit,
-            necessitatibus.
-          </NewsBlock>
-          <NewsBlock headline="This is a headline woohoo" date="August 18, 2022">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
-            oluptatum voluptatem aperiam? Quisquam, maiores animi alias aspernatur
-            voluptates, cumque rerum qui velit perspiciatis saepe possimus? Velit,
-            necessitatibus.
-          </NewsBlock>
-          <NewsBlock headline="This is a headline woohoo" date="August 18, 2022">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
-            oluptatum voluptatem aperiam? Quisquam, maiores animi alias aspernatur
-            voluptates, cumque rerum qui velit perspiciatis saepe possimus? Velit,
-            necessitatibus.
-          </NewsBlock> */}
-          <NewsBlock headline="Dr. Ling Huang Matches to Oncology Fellowship" date="November 30, 2022">
-            Dr. Huang is heading to Wash. U. in St. Louis! Congratulations!
-          </NewsBlock>
-          </div>
+      <div className="grid gap-12 lg:grid-cols-2 2xl:grid-cols-3">
+        {/* add news blocks inside here. top to bottom in code equates to left to right, top to bottom in the website. */}
+        {/* <NewsBlock headline="This is a headline woohoo" date="August 18, 2022">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          oluptatum voluptatem aperiam? Quisquam, maiores animi alias aspernatur
+          voluptates, cumque rerum qui velit perspiciatis saepe possimus? Velit,
+          necessitatibus.
+        </NewsBlock>
+        <NewsBlock headline="This is a headline woohoo" date="August 18, 2022">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          oluptatum voluptatem aperiam? Quisquam, maiores animi alias aspernatur
+          voluptates, cumque rerum qui velit perspiciatis saepe possimus? Velit,
+          necessitatibus.
+        </NewsBlock>
+        <NewsBlock headline="This is a headline woohoo" date="August 18, 2022">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          oluptatum voluptatem aperiam? Quisquam, maiores animi alias aspernatur
+          voluptates, cumque rerum qui velit perspiciatis saepe possimus? Velit,
+          necessitatibus.
+        </NewsBlock>
+        <NewsBlock headline="This is a headline woohoo" date="August 18, 2022">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          oluptatum voluptatem aperiam? Quisquam, maiores animi alias aspernatur
+          voluptates, cumque rerum qui velit perspiciatis saepe possimus? Velit,
+          necessitatibus.
+        </NewsBlock> */}
+        <NewsBlock headline="Dr. Ling Huang Matches to Oncology Fellowship" date="November 30, 2022">
+          Dr. Huang is heading to Wash. U. in St. Louis! Congratulations!
+        </NewsBlock>
       </div>
     </section>
   );
